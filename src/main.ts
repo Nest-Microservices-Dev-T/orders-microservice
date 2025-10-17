08a663d1f9ee6f6a53d52ab0d66ac1594f6ef450
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-16 20:55:46 -0500

    refactor: update the project structure

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Logger } from '@nestjs/common';
 import { envs } from './config';
 
 async function bootstrap() {
-  const logger = new Logger('ORDERS-MICROSERVICE');
+  const logger = new Logger('ORDERS-MICROSERVICE-MAIN');
   const app = await NestFactory.create(AppModule);
   await app.listen(envs.port);
   logger.log(`ORDERS-MS is runing on port: ${envs.port}`)
