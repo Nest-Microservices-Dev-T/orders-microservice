71448ab029c216252891ce6b591fc456e86c1607
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-16 21:07:44 -0500

    feat: (app) Configura app as microservice

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
@@ -5,8 +6,13 @@ import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('ORDERS-MICROSERVICE-MAIN');
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+    transport: Transport.TCP,
+    options: {
+      port: envs.port,
+    }
+  });
+  await app.listen();
   logger.log(`ORDERS-MS is runing on port: ${envs.port}`)
 }
 bootstrap();
