dc66424fcb9768bccbf9d959f3d0981d779f7f4c
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-19 22:04:11 -0500

    feat: (app) implemente pagination in findAll

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from 'generated/prisma';
+import { RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -13,14 +15,53 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   create(createOrderDto: CreateOrderDto) {
-    return createOrderDto;
+    const {status = "PENDING", ...resto} = createOrderDto
+    return this.order.create({
+      data: {
+        status,
+        ...resto
+      }
+    });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+
+    const {limit = 10, page = 1} = orderPaginationDto;
+    const totalPages = await this.order.count({
+      where: {
+        status: orderPaginationDto.status
+      }
+    });
+    const currentPage = page;
+    const perPage = limit;
+    return {
+        data: await this.order.findMany({
+          skip: ( currentPage - 1) * perPage,
+          take: perPage,
+          where:{
+            status: orderPaginationDto.status
+          },
+        }),
+        meta: {
+          total: totalPages,
+          page: currentPage,
+          lastPage: Math.ceil( totalPages / perPage)
+        }
+      }
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string) {
+
+    const order = await this.order.findFirst({
+      where: {id}
+    });
+
+    if(!order) throw new RpcException({ 
+      status: HttpStatus.NOT_FOUND,
+      message: `Order with id: ${id} not found`
+     });
+
+    return order
   }
 }
